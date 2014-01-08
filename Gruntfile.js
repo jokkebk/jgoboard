3d37fcd074f63a08c334da7886c79911f15afcc7
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2014-01-08 23:55:50 +0200

    First Grunt setup with concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,35 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            dist: {
+                src: [ 'src/jgoinit.js', 'src/jgoconstants.js', 'src/jgocoordinate.js',
+                    'src/jgoboard.js', 'src/jgocanvas.js', 'src/jgonotifier.js',
+                    'src/jgosetup.js', 'src/jgonode.js', 'src/jgorecord.js', 'src/jgoutil.js',
+                    'src/jgosgf.js', 'large/board.js' ],
+                dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js',
+            },
+        },
+        //uglify: {
+            //  options: {
+                //    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                //  },
+                //  build: {
+                    //    src: 'src/<%= pkg.name %>.js',
+                    //    dest: 'build/<%= pkg.name %>.min.js'
+                    //  }
+                    //}
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['concat']);
 
 };
